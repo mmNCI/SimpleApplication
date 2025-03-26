06a63e11820e1964f83c4b5a8ae7b9b1e6ed14ad
Mark MacGowan <130069175+MarkMacGowan@users.noreply.github.com>
2025-03-26 21:36:00 +0000

    added deploy file and modified yaml file

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+sudo apt update && sudo apt install nodejs npm
+# Install pm2 which is a production manager for Node.js with a built-in load balencer.
+sudo npm install -g pm2
+# stop any instance of our application running currently
+pm2 stop simple_app
+# change directory into folder where application is downloaded
+cd SimpleApplication
+# Install application dependencies
+npm install
+# Start the application with the process name simple_app using pm2
+pm2 start ./bin/www --name simple_app
