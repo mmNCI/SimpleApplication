f257ecbfae7d399d27c76d7f84a33dd9934ee5f5
Mark MacGowan <130069175+MarkMacGowan@users.noreply.github.com>
2025-03-27 16:18:28 +0000

    fix: removed / in line 8 of deploy file

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ sudo npm install -g pm2
 # stop any instance of our application running currently
 pm2 stop simple_app
 # change directory into folder where application is downloaded
-cd SimpleApplication/
+cd SimpleApplication
 # Install application dependencies
 npm install
 # Start the application with the process name simple_app using pm2
